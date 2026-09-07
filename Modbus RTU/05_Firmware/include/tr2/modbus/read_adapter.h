5e4a08107f90608d3f997a761086fb5033c3b3b6
loloLR17 <215196043+loloLR17@users.noreply.github.com>
2026-09-07 12:23:10 +0200

    Firmware: add P5-G supervision read source

--- Modbus RTU/05_Firmware/include/tr2/modbus/read_adapter.h
+++ Modbus RTU/05_Firmware/include/tr2/modbus/read_adapter.h
@@ -5,6 +5,7 @@
 
 #include "tr2/common/result.h"
 #include "tr2/domain/identity/identity.h"
+#include "tr2/domain/supervision/supervision.h"
 #include "tr2/domain/system_state/system_state.h"
 #include "tr2/domain/time/time_service.h"
 #include "tr2/modbus/register_model.h"
@@ -13,6 +14,7 @@ typedef struct {
     const IdentitySnapshot *identity;
     const SystemStateSnapshot *system_state;
     const TimeSnapshot *time;
+    const SupervisionSnapshot *supervision;
 } ModbusReadSources;
 
 typedef struct {
