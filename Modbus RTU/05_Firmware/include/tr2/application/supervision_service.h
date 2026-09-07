53c3b6060877af6c32047524a28015bbc0c1a4dd
loloLR17 <215196043+loloLR17@users.noreply.github.com>
2026-09-07 12:12:26 +0200

    Firmware: bind P5-E supervision temporal authorities

--- Modbus RTU/05_Firmware/include/tr2/application/supervision_service.h
+++ Modbus RTU/05_Firmware/include/tr2/application/supervision_service.h
@@ -7,15 +7,24 @@
 #include "tr2/common/result.h"
 #include "tr2/domain/acquisition/acquisition.h"
 #include "tr2/domain/supervision/supervision.h"
+#include "tr2/domain/time/time_service.h"
+#include "tr2/platform/monotonic_clock.h"
 
 typedef struct {
     bool initialized;
     bool has_snapshot;
     uint32_t next_calculation_sequence;
+    const MonotonicClock *monotonic_clock;
+    const TimeService *time_service;
     SupervisionSnapshot snapshot;
 } SupervisionService;
 
 Tr2Result supervision_service_init(SupervisionService *service);
+
+Tr2Result supervision_service_bind_temporal_dependencies(
+    SupervisionService *service,
+    const MonotonicClock *monotonic_clock,
+    const TimeService *time_service);
 
 bool supervision_service_is_initialized(const SupervisionService *service);
 
