75de2404c3969ddc9e645489bebf2ea676c20252
loloLR17 <215196043+loloLR17@users.noreply.github.com>
2026-09-07 11:54:45 +0200

    Firmware: add P5-A supervision contracts

---
+++ Modbus RTU/05_Firmware/include/tr2/domain/supervision/supervision.h
@@ -0,0 +1,35 @@
+#ifndef TR2_DOMAIN_SUPERVISION_H
+#define TR2_DOMAIN_SUPERVISION_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "tr2/domain/acquisition/acquisition.h"
+#include "tr2/platform/wall_clock.h"
+
+typedef struct {
+    AcquisitionConfigurationContext configuration;
+    uint32_t calculation_sequence;
+    uint32_t window_duration_ms;
+    uint32_t valid_sample_count;
+
+    uint32_t rms_global_mg;
+    uint32_t peak_global_mg;
+    uint32_t rms_x_mg;
+    uint32_t rms_y_mg;
+    uint32_t rms_z_mg;
+    uint32_t peak_x_mg;
+    uint32_t peak_y_mg;
+    uint32_t peak_z_mg;
+
+    bool values_available;
+    bool window_complete;
+    bool saturation_observed;
+    bool calculation_error;
+
+    MonotonicTimeMs value_monotonic_ms;
+    bool civil_timestamp_available;
+    Tr2CivilTimestamp civil_timestamp;
+} SupervisionSnapshot;
+
+#endif
