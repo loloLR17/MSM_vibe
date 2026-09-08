16b76888ec5b3e238a2efe85a89f094893565399
loloLR17 <215196043+loloLR17@users.noreply.github.com>
2026-09-08 14:16:53 +0200

    Firmware: fix P11-C STM32 HAL configuration

--- Modbus RTU/05_Firmware/platform/stm32/stm32u5xx_hal_conf.h
+++ Modbus RTU/05_Firmware/platform/stm32/stm32u5xx_hal_conf.h
@@ -3,6 +3,7 @@
 
 #define HAL_MODULE_ENABLED
 #define HAL_CORTEX_MODULE_ENABLED
+#define HAL_FLASH_MODULE_ENABLED
 #define HAL_GPIO_MODULE_ENABLED
 #define HAL_PWR_MODULE_ENABLED
 #define HAL_RCC_MODULE_ENABLED
@@ -22,12 +23,16 @@
 #define PREFETCH_ENABLE         0U
 
 #include "stm32u5xx_hal_rcc.h"
+#include "stm32u5xx_hal_flash.h"
 #include "stm32u5xx_hal_gpio.h"
 #include "stm32u5xx_hal_cortex.h"
 #include "stm32u5xx_hal_pwr.h"
 
 #ifdef USE_FULL_ASSERT
+#define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
 void assert_failed(uint8_t *file, uint32_t line);
+#else
+#define assert_param(expr) ((void)0U)
 #endif
 
 #endif
