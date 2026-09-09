b2e859821e2da065aaf59f87aee259ea8ee18a03
loloLR17 <215196043+loloLR17@users.noreply.github.com>
2026-09-09 23:23:20 +0200

    Supervision: fix S7-C1 web host cancellation

--- Modbus RTU/Supervision TR2/src/TR2.Supervision.Web/SupervisionWebHost.cs
+++ Modbus RTU/Supervision TR2/src/TR2.Supervision.Web/SupervisionWebHost.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using TR2.Application;
 
 namespace TR2.Supervision.Web;
@@ -90,6 +91,7 @@ public sealed class SupervisionWebHost
     public async Task RunAsync(CancellationToken cancellationToken)
     {
         await using var application = CreateApplication();
-        await application.RunAsync(cancellationToken);
+        await application.StartAsync(cancellationToken);
+        await application.WaitForShutdownAsync(cancellationToken);
     }
 }
