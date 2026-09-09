c21b94d32e3d9930ec0b4f4735996f091ca86389
loloLR17 <215196043+loloLR17@users.noreply.github.com>
2026-09-09 11:55:34 +0200

    Supervision: add S1-Q telemetry snapshots

---
+++ Modbus RTU/Supervision TR2/src/TR2.Application/DeviceTelemetrySnapshots.cs
@@ -0,0 +1,41 @@
+using TR2.Domain;
+using TR2.Protocol;
+
+namespace TR2.Application;
+
+public sealed record DeviceTelemetrySnapshots(
+    DeviceId DeviceId,
+    ObservedSnapshot<B1SystemState> SystemState,
+    ObservedSnapshot<B2TimeState> TimeState,
+    ObservedSnapshot<B3VibrationSupervision> VibrationState)
+{
+    public static DeviceTelemetrySnapshots Empty(DeviceId deviceId) =>
+        new(
+            deviceId,
+            ObservedSnapshot<B1SystemState>.NeverReceived(),
+            ObservedSnapshot<B2TimeState>.NeverReceived(),
+            ObservedSnapshot<B3VibrationSupervision>.NeverReceived());
+
+    public DeviceTelemetrySnapshots ReceiveSystemState(
+        B1SystemState value,
+        DateTimeOffset receivedAt) =>
+        this with { SystemState = SystemState.Receive(value, receivedAt) };
+
+    public DeviceTelemetrySnapshots ReceiveTimeState(
+        B2TimeState value,
+        DateTimeOffset receivedAt) =>
+        this with { TimeState = TimeState.Receive(value, receivedAt) };
+
+    public DeviceTelemetrySnapshots ReceiveVibrationState(
+        B3VibrationSupervision value,
+        DateTimeOffset receivedAt) =>
+        this with { VibrationState = VibrationState.Receive(value, receivedAt) };
+
+    public DeviceTelemetrySnapshots MarkUnavailable() =>
+        this with
+        {
+            SystemState = SystemState.MarkUnavailable(),
+            TimeState = TimeState.MarkUnavailable(),
+            VibrationState = VibrationState.MarkUnavailable()
+        };
+}
