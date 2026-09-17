e63d87dc00cb1f7dc22b8f0882c5c7a1cbd9a379
loloLR17 <215196043+loloLR17@users.noreply.github.com>
2026-09-17 21:51:02 +0200

    Firmware: add P12-F STM32 serial adapter contract

---
+++ Modbus RTU/05_Firmware/platform/stm32/stm32_serial_transport.h
@@ -0,0 +1,9 @@
+#ifndef TR2_STM32_SERIAL_TRANSPORT_H
+#define TR2_STM32_SERIAL_TRANSPORT_H
+
+#include "tr2/platform/serial_transport.h"
+
+Tr2Result stm32_serial_transport_init(SerialTransport *transport);
+void stm32_serial_transport_irq_handler(void);
+
+#endif
