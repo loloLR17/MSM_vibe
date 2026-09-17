cb499692794b2762ea94bf11e0ed8756d2897f74
loloLR17 <215196043+loloLR17@users.noreply.github.com>
2026-09-17 21:51:35 +0200

    Firmware: route LPUART1 IRQ to P12-F transport

--- Modbus RTU/05_Firmware/platform/stm32/stm32u5xx_it.c
+++ Modbus RTU/05_Firmware/platform/stm32/stm32u5xx_it.c
@@ -1,6 +1,12 @@
 #include "stm32u5xx_hal.h"
+#include "stm32_serial_transport.h"
 
 void SysTick_Handler(void)
 {
     HAL_IncTick();
+}
+
+void LPUART1_IRQHandler(void)
+{
+    stm32_serial_transport_irq_handler();
 }
