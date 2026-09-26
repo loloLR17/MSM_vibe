b89d582abc2a68e87c8023ffb85d02105102fbf9
loloLR17 <215196043+loloLR17@users.noreply.github.com>
2026-09-26 19:16:23 +0200

    Firmware: enable STM32 HAL SPI module

--- Modbus RTU/05_Firmware/platform/stm32/stm32u5xx_hal_conf.h
+++ Modbus RTU/05_Firmware/platform/stm32/stm32u5xx_hal_conf.h
@@ -8,6 +8,7 @@
 #define HAL_GPIO_MODULE_ENABLED
 #define HAL_PWR_MODULE_ENABLED
 #define HAL_RCC_MODULE_ENABLED
+#define HAL_SPI_MODULE_ENABLED
 #define HAL_UART_MODULE_ENABLED
 #define HAL_TIM_MODULE_ENABLED
 
@@ -33,6 +34,7 @@
 #include "stm32u5xx_hal_dma.h"
 #include "stm32u5xx_hal_cortex.h"
 #include "stm32u5xx_hal_pwr.h"
+#include "stm32u5xx_hal_spi.h"
 #include "stm32u5xx_hal_uart.h"
 #include "stm32u5xx_hal_tim.h"
 
